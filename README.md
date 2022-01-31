46651cc8ba71938906a0af138f3dd9e1e38402c5
OmarGard <44001951+OmarGard@users.noreply.github.com>
2022-01-31 16:37:35 -0600

    Add Module3-solution

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ test repo for coursera
 
 1. [Module1-Assignment-Solution](https://omargard.github.io/coursera-test//module1-solution)
 2. [Module2-Assignment-Solution](https://omargard.github.io/coursera-test//module2-solution)
+3. [Module3-Assignment-Solution](https://omargard.github.io/coursera-test//module3-solution)
---
+++ module3-solution/app.js
@@ -0,0 +1,76 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+    .directive('foundItems', foundItemsDirective);
+
+  function foundItemsDirective() {
+    var ddo =
+    {
+      templateUrl: 'foundItems.html',
+      scope: {
+        found: '<',
+        onRemove: '&',
+      },
+      controller: NarrowItDownController,
+      controllerAs: 'ctrl',
+      bindToController: true
+    }
+    return ddo;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService', '$filter'];
+  function NarrowItDownController(MenuSearchService, $filter) {
+    var ctrl = this;
+    ctrl.found = [];
+    ctrl.nothingIsFound = false;
+    ctrl.textToSearch = "";
+    ctrl.search = function () {
+      var filteredTerm = $filter('lowercase')(ctrl.textToSearch);
+      MenuSearchService.getMatchedMenuItems(filteredTerm).then(function (result) {
+        ctrl.found = result;
+        if (result.length == 0)
+          ctrl.nothingIsFound = true;
+        else
+          ctrl.nothingIsFound = false;
+      });
+
+    };
+
+    ctrl.onRemove = function (indexToRemove) {
+      if (ctrl.found.length > 0) {
+        ctrl.found.splice(indexToRemove, 1);
+        if (ctrl.found.length <= 0)
+          ctrl.nothingIsFound = true;
+        else
+          ctrl.nothingIsFound = false;
+      }
+      else {
+        ctrl.nothingIsFound = true
+      }
+    }
+
+  }
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+    service.getMatchedMenuItems = function (searchTerm) {
+      var config = {
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json"),
+      };
+      var foundItems = [];
+      return $http(config).then(function (result) {
+        for (var i = 0; i < result.data.menu_items.length; i++) {
+          if (result.data.menu_items[i].description.indexOf(searchTerm) != -1)
+            foundItems.push(result.data.menu_items[i]);
+        }
+        return foundItems;
+      });
+    };
+  }
+
+})();
